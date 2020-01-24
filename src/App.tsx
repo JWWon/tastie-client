fdc6db8f00d973fc6541a261634035d63921c415
Jiwoon Won <skywrace@gmail.com>
2020-01-24 14:04:11 +0900

    Remove init settings, Install basic packages

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
