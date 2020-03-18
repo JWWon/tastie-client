fc89ef4e227f20b05de211f6a36cf7c5e5f7f4c2
Jiwoon Won <wonjiwn@naver.com>
2020-03-18 23:12:28 +0900

    MA-70 HOTFIX not showing BlurView by remove

--- src/components/templates/Modal/Modal.style.ts
+++ src/components/templates/Modal/Modal.style.ts
@@ -1,18 +1,6 @@
 import styled from 'styled-components/native';
-import {Platform} from 'react-native';
-import {VibrancyView} from '@react-native-community/blur';
 
-// Only for iOS
-export const ModalBlur = styled(VibrancyView).attrs({
-  blurType: 'dark',
-  blurAmount: 4,
-})`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-`;
+// TODO: Add Blur backround
 
 export const ModalTouch = styled.TouchableOpacity.attrs({
   activeOpacity: 1,
@@ -22,7 +10,7 @@ export const ModalTouch = styled.TouchableOpacity.attrs({
   right: 0;
   bottom: 0;
   left: 0;
-  ${Platform.OS === 'android' && 'background: rgba(49, 49, 49, 0.8);'}
+  background: rgba(49, 49, 49, 0.7);
 `;
 
 export const ModalSafeView = styled.SafeAreaView`
--- src/components/templates/Modal/Modal.tsx
+++ src/components/templates/Modal/Modal.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Platform} from 'react-native';
 
 import Dismiss from '@components/atoms/Dismiss';
 import * as s from './Modal.style';
@@ -10,7 +9,6 @@ interface Props {
 
 const Modal: React.FC<Props> = ({onDismiss, children}) => (
   <s.ModalTouch onPress={onDismiss}>
-    {Platform.OS === 'ios' && <s.ModalBlur />}
     <s.ModalSafeView>
       <Dismiss icon="close" onPress={onDismiss} />
     </s.ModalSafeView>
